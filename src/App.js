266dd37d6ab61029d8ad4334f1500b9366834c0f
pawandandotiya <pawan.kdandotiya.chy17@iitbhu.ac.in>
2021-02-19 12:23:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+        <h1>I am a react app.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

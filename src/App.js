1d10c3565f031681c54c733e78f1a368194f4ad2
pawandandotiya <pawan.kdandotiya.chy17@iitbhu.ac.in>
2021-02-20 11:54:41 +0530

    added person component in prson.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person'
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-       
-        <h1>I am a react app.</h1>
+      <h1> Hi, I'm a React App.</h1>
+      <h1>This is really working!!</h1>
+      <Person/>
       </div>
     );
+    //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Hi, I am a React App.',))
   }
 }
 
-export default App;
+export default App; 

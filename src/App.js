30188607da352b4ec81dd876f2380084a5012e59
pawandandotiya <pawan.kdandotiya.chy17@iitbhu.ac.in>
2021-02-25 07:06:42 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react';
+import React {useState} from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
-class App extends Component {
-  state={
-    persons:[
-      {name:'Pawan', age:22},
-      {name:'Aman', age:18},
-      {name:'Rudra', age:2},
-    ]
-  }
-
-  switchNameHandler= () => {
-    //console.log('Was Clicked');
-   // Don't do as:  this.state.persons[0].name='PKD';
-   // Do as follows:
-   this.setState(
-   {
-    persons:[
-      {name:'PKD', age:22},
-      {name:'Aman', age:18},
-      {name:'Rudra', age:3},
-    ]
-   }
-   )
-  }
-
-  render() {
+import { useState } from 'react';
+const app=()=> {
+    useState(
+      {
+        persons:[
+        {name:'Pawan', age:22},
+        {name:'Aman', age:18},
+        {name:'Rudra', age:2},
+       ]
+      }
+    
+    );
     return (
       <div className="App">
       <h1> Hi, I'm a React App.</h1>
@@ -38,7 +25,6 @@ class App extends Component {
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Hi, I am a React App.',))
-  }
-}
+};
 
-export default App; 
+export default app; 
